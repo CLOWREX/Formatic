68c34e673c9964262fa8dbc4e6f3cea84a1c113d
CLOWREX <ariefadiwibowo2@gmail.com>
2026-09-06 09:29:40 +0700

    fix: submitForm pakai event 'submit' bukan 'update' di changeFormSubmit

--- services/form-api/src/submit/submit.service.ts
+++ services/form-api/src/submit/submit.service.ts
@@ -460,7 +460,7 @@ export class SubmitService {
         .first()
       if (existingSubmit.status == "completed") throw new ConflictException("Anda sudah mengisi form ini")
 
-      const updateToCompleted = await this.changeFormSubmit("update", req.id, form.id, 1, req.username, this.knexService.connection.fn.now())
+      const updateToCompleted = await this.changeFormSubmit("submit", req.id, form.id, 1, req.username, this.knexService.connection.fn.now())
 
       await trx('user_answer').insert(
         answers.map((answer) => ({ ...answer, submitted_id: updateToCompleted.id }))
