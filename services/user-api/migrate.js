38bd3a95e2f1b42b4b2d794bebe81c345e008dbe
nabixka <nabixka05@gmail.com>
2026-09-25 14:30:20 +0700

    ATMINNNN

--- services/user-api/migrate.js
+++ services/user-api/migrate.js
@@ -3,7 +3,6 @@ const bcrypt = require("bcrypt")
 
 async function migrate() {
     try {
-
         // Create Database user_db
         await pg.query(`
             CREATE DATABASE formatic_user
@@ -14,6 +13,46 @@ async function migrate() {
             CREATE DATABASE formatic_form
         `)
         console.log("Berhasil Membuat Form DB")
+
+        // Create Table Admin
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS admin(
+            id SERIAL PRIMARY KEY,
+            username VARCHAR,
+            password TEXT
+            )
+        `)
+
+        const hashPassword = await bcrypt.hash('smkn10jktdki', 10)
+        await pool.query(`
+            INSERT INTO admin (username, password) VALUES ('admin123', $1)`,
+        [hashPassword])
+
+        console.log("Berhasil Membuat Admin")
+
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS app_setting(
+            id SERIAL PRIMARY KEY,
+            config VARCHAR UNIQUE,
+            action BOOLEAN DEFAULT TRUE
+            )
+        `)
+
+        console.log("Berhasil Membuat app_setting")
+
+        await pool.query(`
+            INSERT INTO app_setting (config, action) 
+            VALUES ('allow_registration', true) 
+            ON CONFLICT (config) DO NOTHING;
+        `)
+
+        await pool.query(`
+            INSERT INTO app_setting (config, action) 
+            VALUES ('allow_login', true) 
+            ON CONFLICT (config) DO NOTHING;
+        `)
+
+        console.log("Berhasil Menambahkan Default Setting")
 
         // Create Table User
         await pool.query(`
@@ -26,29 +65,6 @@ async function migrate() {
         `)
         console.log("Berhasil Membuat Table User")
 
-        const userModel = [
-            {
-                username: "fadhil hebat", 
-                password: "fadhil123"
-            },
-            {
-                username: "erzy mantap", 
-                password: "erzy123"
-            },
-            {
-                username: "abdi coy",
-                password: "abdi123"
-            }
-        ]
-
-        for (let user of userModel) {
-            const hashPassword = await bcrypt.hash(user.password, 10)
-            await pool.query(`
-                INSERT INTO users (username, password) VALUES ($1, $2)
-            `, [user.username, hashPassword])
-        }
-
-        console.log("Berhasil Membuat Seeder User")
     }
     catch (err) {
         console.log(err)
